4875a1cc118333644ee8874e394d0ca0332b21bb
Даниил <bachurin-danya7267@yandex.ru>
2023-04-01 20:41:07 +0300

    Accounting v.3.0

--- src/ReadDataFromFiles.java
+++ src/ReadDataFromFiles.java
@@ -3,12 +3,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ReadDataFromFiles {
-   /* public String path;
 
-    public ReadDataFromFiles(String path){
-        this.path = path;
-    }
-*/
     public String readFileContentsOrNull(String path){   // Считывание данных с файлов
         try {
             return Files.readString(Path.of(path));
